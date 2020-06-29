4caa521a601ee32b11471e2b2e3ebb4709a54117
Suvarka Piratheepan <parada9nathan@gmail.com>
2020-06-29 12:34:54 +0100

    Movie Api controller added

---
+++ MVCVidly/Controllers/API/MovieController.cs
@@ -0,0 +1,121 @@
+﻿using AutoMapper;
+using MVCVidly.Dtos;
+using MVCVidly.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace MVCVidly.Controllers.API
+{
+    public class MovieController : ApiController
+    {
+        private readonly ApplicationDbContext _context;
+
+        public MovieController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
+
+        //Get/api/Movie
+        //public IEnumerable<Movie> GetMovies()
+        //{
+        //    return _context.Movies.ToList();
+        //}
+        public IEnumerable<MovieDto> GetMovies()
+        {
+            return _context.Movies.ToList().Select(Mapper.Map<Movie,MovieDto>);
+        }
+
+        //Get/api/Movie/1
+        //public Movie GetMovie(int id)
+        //{
+        //    var movie = _context.Movies.SingleOrDefault(m => m.Id == id);
+        //    if (movie == null)
+        //        throw new HttpResponseException(HttpStatusCode.NotFound);
+        //    return movie;
+        //}
+
+        public MovieDto GetMovie(int id)
+        {
+            var movie = _context.Movies.SingleOrDefault(m => m.Id == id);
+            if (movie == null)
+                throw new HttpResponseException(HttpStatusCode.NotFound);
+            return Mapper.Map<Movie,MovieDto>(movie);
+        }
+
+        //Post/api/Movie
+        [HttpPost]
+        //public Movie CreateMovie(Movie movie)
+        //{
+        //    if (movie == null)
+        //        throw new HttpResponseException(HttpStatusCode.NotFound);
+        //    if (!ModelState.IsValid)
+        //        throw new HttpResponseException(HttpStatusCode.BadRequest);
+        //    _context.Movies.Add(movie);
+        //    _context.SaveChanges();
+        //    return movie;
+
+        //}
+        public MovieDto CreateMovie(MovieDto movieDto)
+        {
+            if (movieDto == null)
+                throw new HttpResponseException(HttpStatusCode.NotFound);
+            if (!ModelState.IsValid)
+                throw new HttpResponseException(HttpStatusCode.BadRequest);
+            var movie = Mapper.Map<MovieDto, Movie>(movieDto);
+            _context.Movies.Add(movie);
+            _context.SaveChanges();
+            movieDto.Id = movie.Id;
+            return movieDto;
+
+        }
+
+        //Put/api/Movie/1
+
+        [HttpPut]
+        //public void UpdateMovie(int id,Movie movie)
+        //{
+        //    if (movie == null)
+        //        throw new HttpResponseException(HttpStatusCode.NotFound);
+        //    if (!ModelState.IsValid)
+        //        throw new HttpResponseException(HttpStatusCode.BadRequest);
+        //    var movieInDb = _context.Movies.FirstOrDefault(m => m.Id == id);
+        //    movieInDb.Name = movie.Name;
+        //    movieInDb.GenreId = movie.GenreId;
+        //    movieInDb.DateAdded = movie.DateAdded;
+        //    movieInDb.ReleaseDate = movie.ReleaseDate;
+        //    movieInDb.NumberInStock = movie.NumberInStock;
+        //    _context.SaveChanges();
+        //}
+        public void UpdateMovie(int id, MovieDto movieDto)
+        {
+            if (movieDto == null)
+                throw new HttpResponseException(HttpStatusCode.NotFound);
+            if (!ModelState.IsValid)
+                throw new HttpResponseException(HttpStatusCode.BadRequest);
+            var movieInDb = _context.Movies.FirstOrDefault(m => m.Id == id);
+            Mapper.Map(movieDto, movieInDb);
+            _context.SaveChanges();
+        }
+
+        //Delete/api/Movie/1
+        [HttpDelete]
+
+        public void DeleteMovie(int id)
+        {
+            var movieInDb = _context.Movies.FirstOrDefault(m => m.Id == id);
+            _context.Movies.Remove(movieInDb);
+            _context.SaveChanges();
+        }
+    }
+
+    
+}
---
+++ MVCVidly/Dtos/MovieDto.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MVCVidly.Dtos
+{
+    public class MovieDto
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        
+        public byte GenreId { get; set; }
+       
+        public DateTime? DateAdded { get; set; }
+
+    
+        public DateTime? ReleaseDate { get; set; }
+
+      
+        public byte NumberInStock { get; set; }
+    }
+}
